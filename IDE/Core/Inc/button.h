1c48d26f8eafd21c1a168c399e01f093ae0178a2
MSI\MSI-G <hung.holloyd1501@hcmut.edu.vn>
2023-11-14 19:43:32 +0700

    Init lab3

---
+++ IDE/Core/Inc/button.h
@@ -0,0 +1,17 @@
+/*
+ * button.h
+ *
+ *  Created on: Nov 14, 2023
+ *      Author: MSI-G
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+#define NORMAL_STATE  GPIO_PIN_SET
+#define PRESSED_STATE GPIO_PIN_RESET
+
+void getKeyInput();
+
+
+#endif /* INC_BUTTON_H_ */
